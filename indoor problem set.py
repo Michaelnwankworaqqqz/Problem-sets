6e45c9e2422b1bc07b0f95a4c6ced64b6f1e6c36
Michael  Nwankwo <mikaelicious99@gmail.com>
2025-01-15 06:15:45 -0800

    Create indoor problem set.py
    This program prompts user for a string, and then lowercases all characters of the inputted strings.

---
+++ indoor problem set.py
@@ -0,0 +1,10 @@
+# Define main
+def main():
+    # Prompt user for input
+    user_string = input("Enter a text: ")
+
+# Print out user's input and ensure it's in lowercase
+    print(user_string.lower())
+
+# Call main
+main()
