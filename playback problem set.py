a3d28a8b49e6b1cca1b5709e3a4279091a41677e
Michael  Nwankwo <mikaelicious99@gmail.com>
2025-01-15 06:18:52 -0800

    Create playback problem set.py
    The playback program gets user's input, and replaces every 'space' in the string with '...', using the replace() string method.

---
+++ playback problem set.py
@@ -0,0 +1,9 @@
+# Define main
+def main():
+    # Get user's input
+    user = input("Enter a text: ")
+    # print user's input with ... replacing each space
+    print(user.replace(' ', '...'))
+
+#call main
+main()
